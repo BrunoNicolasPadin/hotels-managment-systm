5517131ddd5f3a3d15d744208d69c2b2d1ae7161
BrunoNicolasPadin <BrunoNicolasPadin@users.noreply.github.com>
2023-06-23 21:33:51 +0000

    PHP Linting (Pint)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,15 +2,14 @@
 
 namespace App\Providers;
 
-use Spatie\Health\Facades\Health;
 use Illuminate\Support\ServiceProvider;
-use Spatie\Health\Checks\Checks\PingCheck;
 use Spatie\Health\Checks\Checks\DatabaseCheck;
+use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
 use Spatie\Health\Checks\Checks\DebugModeCheck;
 use Spatie\Health\Checks\Checks\EnvironmentCheck;
-use Spatie\Health\Checks\Checks\DatabaseSizeCheck;
 use Spatie\Health\Checks\Checks\OptimizedAppCheck;
-use Spatie\SecurityAdvisoriesHealthCheck\SecurityAdvisoriesCheck;
+use Spatie\Health\Checks\Checks\PingCheck;
+use Spatie\Health\Facades\Health;
 
 class AppServiceProvider extends ServiceProvider
 {
