1430189fe4a458dd4eca001ef0b12ec05356ba64
brunonp_ <brunonicolaspadin@gmail.com>
2023-06-22 23:11:33 -0300

    Change exampletest and workflow

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\User;
+it('should return ok', function () {
+    $response = $this->get(route('home'));
 
-it('should show create form', function (User $admin) {
-    $response = $this->actingAs($admin)->get(route('roles.create'));
-
-    $response->assertStatus(200)->assertSeeText('Role Name');
-})->with('admin');
+    $response->assertStatus(200);
+});
