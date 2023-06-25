c1f029dc04e5c810e0ebf0ce06959b6d3ab3c939
brunonp_ <brunonicolaspadin@gmail.com>
2023-06-25 17:21:27 -0300

    CRUD permission

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,63 +2,72 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StorePermissionRequest;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Spatie\Permission\Models\Permission;
 
 class PermissionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
+    public function index(Request $request): View
     {
-        //
+        $params = $request->except('_token');
+        $permissions = null;
+        $pages = 10;
+
+        if (empty($params) || (! isset($params['searchData']) && isset($params['filter']))) {
+            $permissions = Permission::paginate($pages);
+        } elseif (isset($params['filter'])) {
+            if ($params['filter'] === 'name') {
+                $permissions = Permission::where('name', 'LIKE', trim($params['searchData']).'%')->paginate($pages);
+            }
+        } else {
+            $permissions = Permission::paginate($pages);
+        }
+
+        return view('permissions.index', [
+            'permissions' => $permissions,
+            'params' => $params,
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('permissions.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
+    public function store(StorePermissionRequest $request): RedirectResponse
     {
-        //
+        Permission::create($request->validated());
+
+        return redirect()->route('permissions.index')->with(['successMessage' => 'Permission created!']);
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
+    public function edit(string $id): View
     {
-        //
+        return view('permissions.edit', [
+            'permission' => Permission::findOrFail($id),
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
+    public function update(StorePermissionRequest $request, string $id)
     {
-        //
+        $permission = Permission::findOrFail($id);
+        $permission->update($request->validated());
+
+        return redirect()->route('permissions.index')->with(['successMessage' => 'Permission updated!']);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        //
+        Permission::findOrFail($id)->delete($id);
+
+        return redirect()->route('permissions.index')->with(['successMessage' => 'Permission deleted!']);
     }
 }
--- routes/resources/permission.php
+++ routes/resources/permission.php
@@ -3,6 +3,4 @@
 use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('roles/{id}')->group(function () {
-    Route::resource('permissions', PermissionController::class)->except(['show']);
-});
+Route::resource('permissions', PermissionController::class)->except(['show']);
