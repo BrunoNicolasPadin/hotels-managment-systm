60458701889d5ca644a2d01ea68d08e3f6751cd1
brunonp_ <brunonicolaspadin@gmail.com>
2023-06-25 18:53:10 -0300

    CHange route and add link to roles list

--- routes/resources/rolePermission.php
+++ routes/resources/rolePermission.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('roles/{id}/assigned-permissions')->group(function () {
+Route::prefix('roles/{id}')->group(function () {
     Route::resource('assigned-permissions', PermissionController::class)->except(['show', 'edit', 'update']);
 });
 
