d5fceb857f0f8b2f4249d12543ddb6641fad5efb
brunonp_ <brunonicolaspadin@gmail.com>
2023-07-02 19:49:39 -0300

    Change location of the code

--- app/Jobs/CreateProcessJob.php
+++ app/Jobs/CreateProcessJob.php
@@ -7,6 +7,7 @@ use App\Models\Lov;
 use App\Models\Hotel;
 use App\Models\Process;
 use App\Exports\HotelsExport;
+use App\Services\ExportServices;
 use Illuminate\Bus\Queueable;
 use Illuminate\Support\Facades\Log;
 use Maatwebsite\Excel\Facades\Excel;
@@ -36,49 +37,7 @@ class CreateProcessJob implements ShouldQueue
      */
     public function handle()
     {
-        try {
-            $path = null;
-            $name = null;
-            $params = $this->query;
-            $model = $this->process->model->label;
-
-            $today = Carbon::now('America/Argentina/Buenos_Aires')->format('d_m_Y_H_i_s');
-            $name = $this->process->id.'_'.$model.'_'.$today;
-            $path = 'processes/exports/files/'.$name.'.xlsx';
-
-            if ($model === 'Hotel') {
-                if (empty($params) || (! isset($params['searchData']) && isset($params['filter']))) {
-                    $hotels = Hotel::withTrashed()->with('type')->get();
-                } else {
-                    $hotels = Hotel::withTrashed()->with('type')->filter($params)->get();
-                }
-    
-                Excel::store(new HotelsExport($hotels->toArray()), $path, 'public');
-
-                $total = $hotels->count();
-            }
-
-            $this->process->status_id = Lov::where('code', 'STATUS_PROCESS_COMPLETED')->first()->id;
-            $this->process->file = 'public/'.$path;
-            $this->process->total = $total;
-            $this->process->save();
-        } catch (\Throwable $th) {
-            $this->process->status_id = Lov::where('code', 'STATUS_PROCESS_FAILED')->first()->id;
-            $this->process->save();
-
-            Log::error($th->getMessage());
-
-            $file = 'Row,Attribute,Errors,'.PHP_EOL;
-            $file .= $th->getMessage();
-            $path = 'processes/exports/logs'.$name.'_Log.csv';
-
-            //guardo el log en el storage local
-            Storage::putFileAs(
-                'public/processes/exports/logs', $file, $name.'_Log.csv'
-            );
-
-            $this->process->log = $path;
-            $this->process->save();
-        }
+        $exportProcess = new ExportServices;
+        $exportProcess->handle($this->query, $this->process);
     }
 }
